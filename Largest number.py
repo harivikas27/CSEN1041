e47ec85da5c3e1bb2e5ffce85de43690e2b4ea3b
harivikas27 <hpendurt@student.gitam.edu>
2026-02-12 15:36:00 +0530

    Update Largest number.py

--- Largest number.py
+++ Largest number.py
@@ -7,3 +7,10 @@ elif num2 > num1:
     print(f"The largest number is {num2}")
 else:
     print("Both numbers are equal")
+
+
+OUTPUT
+
+Enter first number: 4
+Enter second number: 7
+The largest number is 7.0
